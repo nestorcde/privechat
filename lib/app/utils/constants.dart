903544a9a6b639fc8c3d99bbd44d80f0e4749802
Nestor Espinola <nestorcde@gmail.com>
2021-11-20 11:53:15 -0300

    persmiso internet

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 class Constants {
-  // static const API_BASE_URL_ANDROID_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com/api";
-  // static const API_BASE_URL_IOS_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com/api";
-  // static const SOCKET_BASE_URL_ANDROID_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com";
-  // static const SOCKET_BASE_URL_IOS_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com";
-  static const API_BASE_URL_ANDROID_LOCAL = "http://10.0.2.2:3000/api";
-  static const API_BASE_URL_IOS_LOCAL = "http://localhost:3000/api";
-  static const SOCKET_BASE_URL_ANDROID_LOCAL = "http://10.0.2.2:3000";
-  static const SOCKET_BASE_URL_IOS_LOCAL = "http://localhost:3000";
+  static const API_BASE_URL_ANDROID_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com/api";
+  static const API_BASE_URL_IOS_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com/api";
+  static const SOCKET_BASE_URL_ANDROID_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com";
+  static const SOCKET_BASE_URL_IOS_LOCAL = "https://chat-app-server-nestorcde.herokuapp.com";
+//   static const API_BASE_URL_ANDROID_LOCAL = "http://10.0.2.2:3000/api";
+//   static const API_BASE_URL_IOS_LOCAL = "http://localhost:3000/api";
+//   static const SOCKET_BASE_URL_ANDROID_LOCAL = "http://10.0.2.2:3000";
+//   static const SOCKET_BASE_URL_IOS_LOCAL = "http://localhost:3000";
 }
 
 enum ServerStatus {
